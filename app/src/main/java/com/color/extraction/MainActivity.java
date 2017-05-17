8571da67ac41aa5181b6f646ac25bd5c55985520
大力哥 <137722697@qq.com>
2017-05-17 23:57:38 +0800

    a

--- app/src/main/java/com/color/extraction/MainActivity.java
+++ app/src/main/java/com/color/extraction/MainActivity.java
@@ -1,22 +1,16 @@
 package com.color.extraction;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView tv_rgb;
-    private ImageView iv_image;
-    private Bitmap bitmap;
+    private RGBImageView iv_image;
     private String TAG = "RGBActivity";
     private Button btnColor;
     public static final int NONE = 0;
@@ -39,66 +33,14 @@ public class MainActivity extends AppCompatActivity {
         point = new Paint();
         point.setAntiAlias(true);
         point.setColor(Color.WHITE);
-        iv_image = (ImageView) findViewById(R.id.iv_image);
-        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.demopic);
-        iv_image.setOnTouchListener(new View.OnTouchListener() {
-
+        iv_image = (RGBImageView) findViewById(R.id.iv_image);
+        iv_image.setOnClickListener(new RGBImageView.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                int x = (int) event.getX();
-
-                int y = (int) event.getY();
-                canvas.drawCircle(x, y, 100, point);
-                if (event.getAction() == MotionEvent.ACTION_UP) {
-                    //点击处
-                    int cColor = bitmap.getPixel(x, y);
-                    tvColor.setBackgroundColor(cColor);
-                    // 如果你想做的更细致的话 可以把颜色值的R G B 拿到做响应的处理
-                    int cr = Color.red(cColor);
-                    int cg = Color.green(cColor);
-                    int cb = Color.blue(cColor);
-
-                    //左上角
-                    int ltColor = bitmap.getPixel(x - distance, y - distance);
-                    tvColor.setBackgroundColor(ltColor);
-                    // 如果你想做的更细致的话 可以把颜色值的R G B 拿到做响应的处理
-                    int ltr = Color.red(ltColor);
-                    int ltg = Color.green(ltColor);
-                    int ltb = Color.blue(ltColor);
-
-                    //左下角
-                    int lbColor = bitmap.getPixel(x - distance, y + distance);
-                    tvColor.setBackgroundColor(lbColor);
-                    // 如果你想做的更细致的话 可以把颜色值的R G B 拿到做响应的处理
-                    int lbr = Color.red(lbColor);
-                    int lbg = Color.green(lbColor);
-                    int lbb = Color.blue(lbColor);
-
-                    //右上角
-                    int rtColor = bitmap.getPixel(x + distance, y - distance);
-                    tvColor.setBackgroundColor(rtColor);
-                    // 如果你想做的更细致的话 可以把颜色值的R G B 拿到做响应的处理
-                    int rtr = Color.red(rtColor);
-                    int rtg = Color.green(rtColor);
-                    int rtb = Color.blue(rtColor);
-
-                    //右下角
-                    int rbColor = bitmap.getPixel(x - distance, y + distance);
-                    tvColor.setBackgroundColor(rbColor);
-                    // 如果你想做的更细致的话 可以把颜色值的R G B 拿到做响应的处理
-                    int rbr = Color.red(rbColor);
-                    int rbg = Color.green(rbColor);
-                    int rbb = Color.blue(rbColor);
-                    int sumr = (int) ((cr + ltr + rbr + lbr + rtr) / 5.0);
-                    int sumg = (int) ((cg + ltg + rbg + lbg + rtg) / 5.0);
-                    int sumb = (int) ((cb + ltb + rbb + lbb + rtb) / 5.0);
-
-                    tvR.setText(sumr + "");
-                    tvG.setText(sumg + "");
-                    tvB.setText(sumb + "");
-                    iv_image.draw(canvas);
-                }
-                return true;
+            public void onClick(int color, int r, int g, int b) {
+                tvR.setText(r + "");
+                tvG.setText(g + "");
+                tvB.setText(b + "");
+                tvColor.setBackgroundColor(color);
             }
         });
     }
--- app/src/main/java/com/color/extraction/RGBImageView.java
+++ app/src/main/java/com/color/extraction/RGBImageView.java
@@ -1,8 +1,163 @@
 package com.color.extraction;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+import com.hdl.elog.ELog;
+
 /**
- * Created by HDL on 2017/5/17.
+ * RGB颜色提取
  */
 
-public class RGBImageView {
+public class RGBImageView extends ImageView {
+    private Paint mPaint;
+    private Bitmap bitmap;
+    private int distance = 50 / 2;
+    private int smallRudius = 5;
+    private int bigRudius = 10;
+
+    private OnClickListener listener;
+    private float curX, curY;
+    private float ltX, ltY;
+    private float lbX, lbY;
+    private float rtX, rtY;
+    private float rbX, rbY;
+
+    public RGBImageView(Context context) {
+        this(context, null);
+    }
+
+    public RGBImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RGBImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        bitmap = ((BitmapDrawable) this.getDrawable()).getBitmap();
+        mPaint.setColor(Color.WHITE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawLine(-getMeasuredWidth() / 2, 0, getMeasuredWidth() / 2, 0, mPaint);
+        canvas.drawLine(0, getMeasuredHeight() / 2, 0, getMeasuredHeight() / 2, mPaint);
+        if (curX != 0) {
+            canvas.drawCircle(curX, curY, bigRudius, mPaint);
+            canvas.drawCircle(ltX, ltY, smallRudius, mPaint);
+            canvas.drawCircle(lbX, lbY, smallRudius, mPaint);
+            canvas.drawCircle(rtX, rtY, smallRudius, mPaint);
+            canvas.drawCircle(rbX, rbY, smallRudius, mPaint);
+        }
+    }
+
+    /**
+     * 获取平均颜色
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    public int[] getAvgColor(float x, float y, int redius) {
+        int x1 = (int) (x - redius / 1.44);
+        int y1 = (int) (y - redius / 1.44);
+        int sumr = 0;
+        int sumg = 0;
+        int sumb = 0;
+        int count = 0;
+        for (int i = x1; i < x + redius / 1.44; i++) {
+            for (int j = y1; j < y + redius / 1.44; j++) {
+                count++;
+                sumr += Color.red(bitmap.getPixel(i, j));
+                sumg += Color.green(bitmap.getPixel(i, j));
+                sumb += Color.blue(bitmap.getPixel(i, j));
+            }
+        }
+        int buf[] = {sumr / count, sumg / count, sumb / count};
+        return buf;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            float x = event.getX();
+            float y = event.getY();
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                curY = y;
+                curX = x;
+                //点击处
+                int buf[] = getAvgColor(x, y, bigRudius);
+                // 如果你想做的更细致的话 可以把颜色值的R G B 拿到做响应的处理
+                int cr = buf[0];
+                int cg = buf[1];
+                int cb = buf[2];
+
+                //左上角
+                ltX = x - distance;
+                ltY = y - distance;
+                int ltColor[] = getAvgColor(ltX, ltY, smallRudius);
+
+
+                int ltr = ltColor[0];
+                int ltg = ltColor[1];
+                int ltb = ltColor[2];
+
+
+                //左下角
+                lbX = x - distance;
+                lbY = y + distance;
+
+                int lbColor[] = getAvgColor(lbX, lbY, smallRudius);
+                // 如果你想做的更细致的话 可以把颜色值的R G B 拿到做响应的处理
+                int lbr = lbColor[0];
+                int lbg = lbColor[1];
+                int lbb = lbColor[2];
+                //右上角
+                rtX = x + distance;
+                rtY = y - distance;
+                int rtColor[] = getAvgColor(rtX, rtY, smallRudius);
+                // 如果你想做的更细致的话 可以把颜色值的R G B 拿到做响应的处理
+                int rtr = rtColor[0];
+                int rtg = rtColor[1];
+                int rtb = rtColor[2];
+                //右下角
+                rbX = x + distance;
+                rbY = y + distance;
+                int rbColor[] = getAvgColor(rbX, rbY, smallRudius);
+                // 如果你想做的更细致的话 可以把颜色值的R G B 拿到做响应的处理
+                int rbr = rbColor[0];
+                int rbg = rbColor[1];
+                int rbb = rbColor[2];
+                int sumr = (int) ((cr + ltr + rbr + lbr + rtr) / 5.0);
+                int sumg = (int) ((cg + ltg + rbg + lbg + rtg) / 5.0);
+                int sumb = (int) ((cb + ltb + rbb + lbb + rtb) / 5.0);
+                listener.onClick(Color.argb(255,sumr,sumg,sumb), sumr, sumg, sumb);
+                ELog.hdl("圆心坐标为：" + curX + "--" + curY);
+                postInvalidate();
+            }
+        }
+        return true;
+    }
+
+
+    public void setOnClickListener(OnClickListener listener) {
+        this.listener = listener;
+    }
+
+    interface OnClickListener {
+        void onClick(int color, int r, int g, int b);
+    }
 }
